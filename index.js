4a3919059204c329381eae4cfe1c43bad66ed7d6
pulak01 <pulakkar.mysoftheaven@gmail.com>
2024-05-30 16:40:30 +0600

    get_all_products

--- index.js
+++ index.js
@@ -5,18 +5,39 @@ const app = express()
 
 app.use(express.json()) // 1. Add Middleware
 
+
+// ----------------start server-----------------------
 app.listen(3000, () => {
-    console.log("Server Started") // 3. Start Server
+    console.log("Server Started") // 2. Start Server
 })
 
-app.get('/', function (req, res) {
-    res.send('Hello World from express application')
-}) // 2. Add Routes 
 
-app.post('/api/products', async (req, res) =>{ // 4. Add Product
+
+
+// ----------------routes-----------------------
+app.get('/', function (req, res) { // 3. Add Routes
+    res.send('Hello World from express application')
+}) // 3. Add Routes 
+
+
+
+
+// ----------------get all products-----------------------
+app.get('/api/products', async (req, res) => {  // 3. Add Routes to get all products
     try {
-        const product = await Product.create(req.body); // 5. Create Product from Request
-        res.status(200).json(product); // 6. Send Product Response 
+        const products = await Product.find(); // 4. Find Product from DB
+        res.status(200).json(products); // 5. Send Product Response
+    } catch (error) {
+        res.status(500).json({ message: error.message }); // 6. Send Error
+    }
+})
+
+
+// ----------------add product-----------------------
+app.post('/api/products', async (req, res) => {    // 4. Add Product into DB //controller
+    try {
+        const product = await Product.create(req.body);  // 5. Create Product from Request saved in DB
+        res.status(200).json(product);  // 6. Send Product Response
 
     } catch (error) {
         res.status(500).json({ message: error.message }); // 7. Send Error
@@ -29,7 +50,7 @@ app.post('/api/products', async (req, res) =>{ // 4. Add Product
 
 
 
-
+// ----------------connect to DB-----------------------
 //mongodb+srv://pulakkar85:wLWV1QdwQcXu4nX0@backenddb.fwvbaqq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB
 mongoose.connect("mongodb://localhost:27017/pulok_db")
     .then(() => {
@@ -38,3 +59,4 @@ mongoose.connect("mongodb://localhost:27017/pulok_db")
     .catch((e) => {
         console.log("connected error", e);
     }); 
+
